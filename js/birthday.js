dceed1c579521e4fb0419ab5bc68d1d2a8f4dafe
Elijah Littell <elittell214@gmail.com>
2023-06-19 19:33:23 -0400

    Changes

--- js/birthday.js
+++ js/birthday.js
@@ -1,9 +1,13 @@
-var currentDate = new Date();
-var birthday = new Date("09/23/2004");
+let currentDate = new Date();
+let birthday = new Date("09/23/2004");
 
 // Gets difference of time and is in milliseconds
-var timeDifference = currentDate - birthday;
+let timeDifference = currentDate - birthday;
 // Convert milliseconds to years and rounds down
-var age = Math.floor(timeDifference / 1000 / 60 / 60 / 24 / 365);
+let age = Math.floor(timeDifference / 1000 / 60 / 60 / 24 / 365);
 
-document.getElementById("age").innerText = age;
+let allAgeSpans = document.querySelectorAll(".age");
+
+allAgeSpans.forEach(element => {
+    element.innerHTML = age;
+});
